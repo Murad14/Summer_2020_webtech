d62c5c4409cb32df4793f306dd80f2bd39c2b797
Murad Ul Alam <muradulalam4@gmail.com>
2020-09-02 11:33:27 +0600

    Final_Lab_Task_3

---
+++ FINAL_LAB_TASK_3/userhome.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <script type="text/javascript">
+  function call_ajex()
+  {
+    
+  }
+
+  </script>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Welcome Anne!
+</body>
+</html>
