f53f9e39a4aa07d031dc1ea5789566bbe58a2ecc
Murad Ul Alam <muradulalam4@gmail.com>
2020-09-07 12:42:23 +0600

    3rd commit

---
+++ FINAL_EXAM_LAB/admin/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin </title>
+</head>
+<body>
+<center>
+
+	<h1>Admin Page</h1>
+	<a href="../views/adminlogin.php">Login</a> |
+	<a href="../views/adminsignup.php">Signup</a>
+	</center>
+
+</body>
+</html>
---
+++ FINAL_EXAM_LAB/system.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 07, 2020 at 08:31 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `final_lab_exam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system`
+--
+
+CREATE TABLE `system` (
+  `username` varchar(20) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `companyname` varchar(50) NOT NULL,
+  `contact` varchar(11) NOT NULL,
+  `employername` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
