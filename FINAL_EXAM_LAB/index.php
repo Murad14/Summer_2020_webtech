71c4c2921633fb83462f925b502ace6af2a62db7
Murad Ul Alam <muradulalam4@gmail.com>
2020-09-07 12:54:26 +0600

    4th commit

---
+++ FINAL_EXAM_LAB/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index page</title>
+</head>
+<body>
+<center>
+
+	<h1>Employee page</h1>
+	<a href="views/login.php">Login</a>
+
+</center>
+
+</body>
+</html>
